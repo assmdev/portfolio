39f5a7c90f2c2c2dbe58d29a30d1cf53c8a48700
assmdev <assmaa.chrouh@gmail.com>
2024-09-30 21:33:24 +0100

    portfolio intialisation

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+
+//******************************************************* */
+
+// let myForm = document.getElementById("myForm");
+// var errorMessages = document.querySelectorAll("#myForm span");
+
+// myForm.addEventListener("submit", function (event) {
+//     event.preventDefault();
+
+//     const { username, email, subject, message } = myForm.elements;
+
+//     // Clear previous error messages
+//     errorMessages.textContent = "";
+
+//     //validation
+//     if (!username.value.trim()) {
+//         return displayError("Username is required.");
+//     }
+
+//     if (!email.value.trim() || !isValidEmail(email.value)) {
+//         return displayError("Please enter a valid email address.");
+//     }
+
+//     if (!subject.value.trim() || !subject.value.length>=8) {
+//         return displayError("subject must be at least 8 characters");
+//     }
+
+//     if (!message.value.trim()) {
+//         return displayError("Message is required");
+//     }
+
+//     // If validation passes, submit the form
+//     myForm.submit();
+
+// });
+
+// function displayError(msg) {
+//     errorMessages.textContent= `${msg}`;
+// }
+
+// function isValidEmail(email) {
+//     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+// }
+    
+//******************************************************** */
+
+
